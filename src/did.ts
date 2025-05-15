5eab45ae066b2b92619531fa1f7d73b7e3fa188e
Geiner Grandez Valle <ggrandez@emeal.nttdata.com>
2025-05-15 10:05:39 -0500

    chore: add did-base

--- src/did.ts
+++ src/did.ts
@@ -1,11 +1,14 @@
 import { EthrDID as EthrDIDBase } from 'ethr-did'
 import { RemoveFirstParameter } from './types'
+import {Issuer, IssuerEcAlg} from '@kaytrust/did-base'
 
 type EthrDIDConstructParams = ConstructorParameters<typeof EthrDIDBase>
 export type DIDConfig = ConstructorParameters<typeof EthrDIDBase>[0]
 
-export class EthrDID extends EthrDIDBase {
+export class EthrDID extends EthrDIDBase implements Issuer<IssuerEcAlg> {
     setOwner!: EthrDIDBase["changeOwner"]
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    declare signer: Issuer["signer"]
     constructor(...params: EthrDIDConstructParams) {
         super(...params)
         this.setOwner = this.changeOwner.bind(this);
